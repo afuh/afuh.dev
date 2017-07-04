aef3c0df73d4218b230f3c00dc298abb659a481f
Axel <axelfuh@gmail.com>
2017-07-04 16:16:04 +0200

    🌙

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Nav from './Nav';
 import Projects from './Projects';
 import Project from './Project';
-import Readme from './Readme';
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
         <Nav />
         <Switch>
           <Route exact path="/" component={Projects} />
-          <Route path="/readme" component={Readme} />
           <Route path="/p/:lang" component={Projects} />
           <Route path="/p/:lang/:id" component={Project} />
           <Route render={() => <p>page not found T_T </p> } />
