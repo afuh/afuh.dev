e0efe6dc76305ef3027ebb00d16ff34e4d7d0ded
Axel <axelfuh@gmail.com>
2018-08-12 18:22:14 +0200

    Remove google fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,17 +3,11 @@ import Typography from "typography"
 const family = ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', 'sans-serif']
 
 const typography = new Typography({
-  googleFonts: [
-    {
-      name: 'Raleway',
-      styles: ['700', '500', '400', '200']
-    }
-  ],
   baseFontSize: "18px",
   baseLineHeight: 1.6,
   scaleRatio: 2,
-  headerFontFamily: ["Raleway", ...family],
-  bodyFontFamily: ["Raleway", ...family],
+  headerFontFamily: family,
+  bodyFontFamily: family,
   headerColor: '#fff',
   bodyColor: '#fff',
   bodyWeight: '400'
