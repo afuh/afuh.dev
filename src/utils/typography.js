764746c2beb3a7bd75ebe0ee4b2888965e8b50be
Axel <axelfuh@gmail.com>
2018-08-12 11:59:39 +0200

    Update fontfamily

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,11 +3,19 @@ import Typography from "typography"
 const family = ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', 'sans-serif']
 
 const typography = new Typography({
+  googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: ['700', '400', '200']
+    }
+  ],
   baseFontSize: "18px",
   baseLineHeight: 1.6,
   scaleRatio: 2,
-  headerFontFamily: family,
+  headerFontFamily: ['Montserrat', ...family],
   bodyFontFamily: family,
+  headerColor: '#fff',
+  bodyColor: '#fff',
   bodyWeight: '400'
 })
 
