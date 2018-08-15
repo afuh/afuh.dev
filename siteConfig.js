c1f04faf1da1ac72e3d8dbaf7ee61a8dd42737ed
Axel <axelfuh@gmail.com>
2018-08-15 16:58:35 +0200

    Update twitter link

--- siteConfig.js
+++ siteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteUrl: 'https://axelfuhrmann.com',
   favicon: '/images/icon-32x32.png',
   cover: '/images/cover-1440x720.jpg',
-  userTwitter: 'axelfuh',
+  userTwitter: 'rickandmortyapi',
   themeColor: '#000',
   backgroundColor: '#fff',
   working: {
@@ -12,7 +12,7 @@ module.exports = {
   },
   social: [
     { icon: "Github", url: 'https://github.com/afuh' },
-    { icon: "Twitter", url: 'https://twitter.com/axelfuh' },
+    { icon: "Twitter", url: 'https://twitter.com/rickandmortyapi' },
     { icon: "Codepen", url: 'https://codepen.io/mage20' },
     { icon: "Linkedin", url: 'https://www.linkedin.com/in/axel-fuh' },
     { icon: "Envelope", url: 'mailto:axelfuh@gmail.com' }
