89d087b1ab9208c20e8b9ea5208af4f6eb491d4c
Axel <axelfuh@gmail.com>
2018-09-19 16:13:24 -0300

    Add gatsby remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-styled-components`,
+    'gatsby-transformer-remark',
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-typography`,
