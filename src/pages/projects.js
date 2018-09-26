6cc5b2e2af07aff4f1f6603f74af33c27966f685
Axel <axelfuh@gmail.com>
2018-09-26 11:12:00 -0300

    Add gatsby image

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,35 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql, Link } from "gatsby"
+import Img from 'gatsby-image'
 import styled from "styled-components"
 
 import { flex } from 'utils/styles'
-
-import Layout from 'components/layout'
 
 const Box = styled.li`
   ${flex({ x: 'flex-start' })}
 `
 
-const Projects = ({ location, data }) => (
-  <Layout location={location}>
-    <ul>
-      {data.allContentfulProject.edges.map(edge => (
-        <Box key={edge.node.slug} >
-          <div style={{ marginRight: 20 }}>
-            <Link to={edge.node.slug}>
-              <img src={edge.node.image.fixed.src}/>
-            </Link>
-
-          </div>
-          <div>
-            <Link to={edge.node.slug}>{edge.node.title}</Link>
-            <p>{edge.node.content.childMarkdownRemark.excerpt}</p>
-          </div>
-        </Box>
-      ))}
-    </ul>
-  </Layout>
+const Projects = ({ data: { allContentfulProject: projects } }) => (
+  <ul>
+    {projects.edges.map(({ node: project }) => (
+      <Box key={project.slug} >
+        <div style={{ marginRight: 20 }}>
+          <Link to={project.slug}>
+            <Img fixed={project.image.fixed}/>
+          </Link>
+        </div>
+        <div>
+          <Link to={project.slug}>{project.title}</Link>
+          <p>{project.content.childMarkdownRemark.excerpt}</p>
+        </div>
+      </Box>
+    ))}
+  </ul>
 )
 
 Projects.propTypes = {
@@ -47,8 +43,11 @@ export const pageQuery = graphql`
           title
           slug
           image {
-            fixed(width: 120) {
+            fixed(width: 200) {
+              height
+              width
               src
+              srcSet
             }
           }
           content {
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,16 +1,23 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import Img from 'gatsby-image'
 import { graphql } from "gatsby"
 import PropTypes from 'prop-types'
 
-const Project = ({ data }) => (
-  <div>
-    {data.allContentfulProject.edges.map(edge =>
-      <Fragment key={edge.node.slug}>
-        <p>{edge.node.title}</p>
-        <div dangerouslySetInnerHTML={{ __html: edge.node.content.childMarkdownRemark.html }} />
-      </Fragment>
-    )}
-  </div>
+const Project = ({ data: { contentfulProject: project } }) => (
+  <>
+    <h1>{project.title}</h1>
+    <div style={{ maxWidth: 960 }}>
+      <Img
+        fluid={project.image.fluid}
+        alt={project.title}
+      />
+    </div>
+    <div>
+      <a href={project.url}>Live</a>
+      <a href={project.code}>Code</a>
+    </div>
+    <div dangerouslySetInnerHTML={{ __html: project.content.childMarkdownRemark.html }} />
+  </>
 )
 
 Project.propTypes = {
@@ -21,16 +28,24 @@ export default Project
 
 export const pageQuery = graphql`
   query($slug: String!) {
-    allContentfulProject(filter: { slug: { eq: $slug } }) {
-      edges {
-        node {
-          title
-          slug
-          content {
-            childMarkdownRemark {
-              html
-            }
-          }
+    contentfulProject(slug: { eq: $slug }) {
+      title
+      url
+      code
+      tags
+      featured
+      image {
+        fluid(maxWidth: 960) {
+          base64
+          srcSet
+          sizes
+          src
+          aspectRatio
+        }
+      }
+      content {
+        childMarkdownRemark {
+          html
         }
       }
     }
