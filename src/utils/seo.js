b39eefe8d773326c80ff0695fded435cb86358cf
Axel <axelfuh@gmail.com>
2019-04-23 21:24:57 +0200

    Update SEO

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,64 +1,64 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 
-const SEO = ({ title, description, image, pathname }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        meta: {
-          defaultTitle,
-          defaultDescription,
-          defaultImage,
-          siteUrl,
-          favicon,
-          userTwitter
-        }
-      }
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: image ? `https:${image}` : `${siteUrl}${defaultImage}`,
-        url: `${siteUrl}${pathname || '/'}`,
-        favicon: `${siteUrl}${favicon}`
-      }
+import { useSiteMeta } from './hooks'
 
-      return (
-        <Helmet
-          htmlAttributes={{ lang: "en" }}
-          title={seo.title}
-        >
-          <link rel="shortcut icon" href={seo.favicon}/>
-          <link rel="icon" href={seo.favicon}/>
+const checkUrl = url => url.match(/^https/i) ? url : "https:" + url
 
-          <meta name="description" content={seo.description} />
-          <meta name="image" content={seo.image} />
+const SEO = ({ title, description, image, pathname }) => {
+  const {
+    title: defaultTitle,
+    description: defaultDescription,
+    image: defaultImage,
+    siteUrl,
+    external
+  } = useSiteMeta()
 
-          <meta property="og:locale" content='en'/>
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content={seo.title} />
-          <meta property="og:title" content={seo.title} />
-          <meta property="og:url" content={seo.url} />
-          <meta property="og:description" content={seo.description} />
-          <meta property="og:image" content={seo.image} />
-          <meta property="og:image:type" content="image/jpeg" />
-          <meta property="og:image:width" content="1440" />
-          <meta property="og:image:height" content="720" />
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    url: `${siteUrl}${pathname || '/'}`,
+    image: {
+      url: image ? checkUrl(image.url) : `${siteUrl}${defaultImage}`,
+      contentType: image ? image.contentType : "image/jpeg",
+      size: image ? image.size : { width: "720", height: "360" }
+    }
+  }
 
-          {userTwitter && <meta name="twitter:creator" content={userTwitter} />}
-          {userTwitter && <meta name="twitter:site" content={userTwitter} />}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={seo.title} />
-          <meta name="twitter:url" content={seo.url} />
-          <meta name="twitter:description" content={seo.description} />
-          <meta name="twitter:image" content={seo.image} />
-        </Helmet>
-      ) }}
-  />
-)
+  const twitterUrl = external.find(item => item.name === 'Twitter').url.split("/")
+  const twitterUser = "@" + twitterUrl[twitterUrl.length - 1]
+
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang: "en" }}
+      title={seo.title}
+    >
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image.url} />
+
+      <meta property="og:locale" content='en'/>
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={seo.title} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image.url} />
+      <meta property="og:image:type" content={seo.image.contentType} />
+      <meta property="og:image:width" content={seo.image.size.width} />
+      <meta property="og:image:height" content={seo.image.size.height} />
+
+      <meta name="twitter:creator" content={twitterUser} />
+      <meta name="twitter:site" content={twitterUser} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image.url} />
+    </Helmet>
+  )
+}
 
 SEO.propTypes = {
   title: PropTypes.string,
@@ -75,18 +75,3 @@ SEO.defaultProps = {
 }
 
 export default SEO
-
-const query = graphql`
-  {
-    site {
-      meta: siteMetadata {
-        defaultTitle: title
-        defaultDescription: description
-        siteUrl
-        defaultImage: image
-        favicon
-        userTwitter
-      }
-    }
-  }
-`
