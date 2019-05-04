bff51a4ac01140e578349b9048af382025ba8396
Axel <axelfuh@gmail.com>
2019-05-04 15:56:46 +0200

    update site config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        color: siteConfig.backgroundColor,
+        color: siteConfig.accent,
         showSpinner: false
       }
     },
