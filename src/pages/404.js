6d4a69d7cf8c7b6f6f2d17972f111624bf16f81d
Axel <axelfuh@gmail.com>
2019-05-08 21:10:14 +0200

    Add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Error from '../components/error'
 
-const NotFoundPage = () => <Layout heading='404' />
+const NotFoundPage = () => (
+  <Layout heading='404'>
+    <Error />
+  </Layout>
+)
 
 export default NotFoundPage
