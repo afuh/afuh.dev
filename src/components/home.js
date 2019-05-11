a408bc71e155a11a630f17895cb5859e2a94705a
Axel <axelfuh@gmail.com>
2019-05-11 09:17:46 +0200

    Remove the github box for now

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import GithubBox from '../components/githubBox'
 import { List } from '../utils/UI'
 
 const Home = ({ data }) => (
-  <>
-    <List data={data} />
-    <GithubBox />
-  </>
+  <List data={data} />
 )
 
 Home.propTypes = {
