e19b43e7c6fdeda36e2be057d99a8a070ee6476a
Axel <axelfuh@gmail.com>
2019-05-26 12:20:12 +0200

    Fix margin issue

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types'
 import { Inner, MarkdownWrapper } from '../utils/UI'
 
 const About = ({ text }) => (
-  <Inner margin>
+  <Inner
+    margin
+    css='margin-bottom: 40px;'
+  >
     <MarkdownWrapper
       as='section'
       html={text.md.html}
