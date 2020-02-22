20873bb08b1a000d423b1ce36b2af5c11c023bf9
Axel <axelfuh@gmail.com>
2020-02-22 23:45:21 +0100

    update 404

--- src/components/error.js
+++ src/components/error.js
@@ -1,71 +1,46 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled, { css } from 'styled-components'
 
 const Wrapper = styled.div`
   ${({ theme }) => theme && css`
     min-height: calc(100vh - ${theme.headerHeight.desktop}px);
   `};
-
   padding: 10px 30px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  p {
+  h2 {
     text-align: center;
-    font-size: 1.8rem;
-    font-weight: 500;
-  }
-
-  #image-wrapper {
-    width: 300px;
-    height: 300px;
+    font-size: 3rem;
+    font-weight: 700;
+    max-width: 640px;
   }
 `
 
-const ImageWrapper = styled.div.attrs({
-  id: 'image-wrapper'
-})`
-  position: relative;
-  border: 10px solid #fff;
-
-  .placeholder {
-    opacity: ${p => p.isLoading ? 1 : 0};
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: #f6f6f680;
-  }
+const ImageWrapper = styled.div`
+  width: 300px;
+  height: 300px;
 
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
     object-position: center center;
-    opacity: ${p => p.isLoading ? 0 : 1};
-    transition: opacity 1s;
   }
 `
 
-const ErrorPage = () => {
-  const [isLoading, setLoading] = useState(true)
-
-  return (
-    <Wrapper>
-      <p>The page you are looking for doesn&apos;t exist, but here is a cute cat for you.</p>
-      <ImageWrapper isLoading={isLoading}>
-        <div className='placeholder'/>
-        <img
-          onLoad={() => setLoading(false)}
-          src='https://source.unsplash.com/random/300/?cat,cats'
-          alt='🐈'
-        />
-      </ImageWrapper>
-    </Wrapper>
-  )
-}
+const ErrorPage = () => (
+  <Wrapper>
+    <h2>
+      The page you are looking for doesn&apos;t exist,
+      but here is a cute cat for you.
+    </h2>
+    <ImageWrapper>
+      <img src='https://source.unsplash.com/random/300/?cat,cats' alt='🐈'/>
+    </ImageWrapper>
+  </Wrapper>
+)
 
 export default ErrorPage
