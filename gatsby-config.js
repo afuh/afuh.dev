35ea892078a28d6beae63567da4cab995ad84581
Axel <axelfuh@gmail.com>
2020-02-22 21:05:31 +0100

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,8 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.SPACE_ID,
-        accessToken: process.env.TOKEN
+        accessToken: process.env.TOKEN,
+        host: (process.env.ENABLE_GATSBY_REFRESH_ENDPOINT ? 'preview' : 'cdn') + '.contentful.com'
       }
     },
     'gatsby-plugin-offline',
