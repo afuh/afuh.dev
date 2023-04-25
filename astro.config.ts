3a4988c35c01d7868e32d152b53a31c0a7da27fa
Axel <me@afuh.dev>
2023-04-25 21:18:34 +0200

    feat: add prefetch

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import image from '@astrojs/image'
 
+import prefetch from '@astrojs/prefetch'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://afuh.dev',
   integrations: [
     sitemap(),
+    prefetch(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
